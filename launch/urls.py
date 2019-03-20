a49f84030df81e5102ca25c7b692f9bba90c6a42
Ali Ahad <ali_ahad@Alis-MacBook-Pro.local>
2019-03-20 23:27:18 +0800

    Change application launch page for redirecting towards restaurant and customer separately

--- launch/urls.py
+++ launch/urls.py
@@ -6,7 +6,11 @@ app_name = "launch"
 
 urlpatterns = [
     path('', views.home, name='launch-home'),
-    path('register/', views.register, name='launch-register'),
-    path('login/', auth_views.LoginView.as_view(template_name='launch/login.html'), name='launch-login'),
+    path('restaurant-launch/', views.restaurant_launch, name='launch-restaurant'),
+    path('customer-launch/', views.customer_launch, name='launch-customer'),
+    path('restaurant-register/', views.register_restaurant, name='launch-restaurant-register'),
+    path('restaurant-login/', auth_views.LoginView.as_view(template_name='launch/restaurant-login.html'), name='launch-restaurant-login'),
+    path('customer-register/', views.register_customer, name='launch-customer-register'),
+    path('customer-login/', auth_views.LoginView.as_view(template_name='launch/customer-login.html'), name='launch-customer-login'),
 ]
 
--- launch/views.py
+++ launch/views.py
@@ -5,14 +5,32 @@ from .forms import RegisterUser
 def home(request):
    return render(request, 'launch/launch.html')
 
-def register(request):
+def restaurant_launch(request):
+   return render(request, 'launch/restaurant-launch.html')
+
+def customer_launch(request):
+   return render(request, 'launch/customer-launch.html')
+
+def register_restaurant(request):
    if request.method == 'POST':
       form = RegisterUser(request.POST)
       if form.is_valid():
          form.save()
          username = form.cleaned_data.get('username')
          messages.success(request, f'Account created for {username}!')
-         return redirect('launch:launch-login')
+         return redirect('launch:launch-restaurant-login')
    else:
       form = RegisterUser()
-   return render(request, 'launch/register.html', {'form': form})
+   return render(request, 'launch/restaurant-register.html', {'form': form})
+
+def register_customer(request):
+   if request.method == 'POST':
+      form = RegisterUser(request.POST)
+      if form.is_valid():
+         form.save()
+         username = form.cleaned_data.get('username')
+         messages.success(request, f'Account created for {username}!')
+         return redirect('launch:launch-customer-login')
+   else:
+      form = RegisterUser()
+   return render(request, 'launch/customer-register.html', {'form': form})
